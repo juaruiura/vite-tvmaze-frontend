2084db85d212fb40f98de41e821537b7a646ff14
Juan Ruiz <ruizuranojuanmanuel@gmail.com>
2025-09-09 11:32:31 +0100

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@core': '/src/core',
+      '@ui': '/src/ui',
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
